92c50e0380f527bd86d144d7b4057493bed0d5b5
beanzinu <73530944+beanzinu@users.noreply.github.com>
2022-09-26 13:47:21 +0900

    add : 프로젝트 생성

---
+++ src/main/java/com/helloworldweb/helloworld_post/HelloworldPostApplication.java
@@ -0,0 +1,13 @@
+package com.helloworldweb.helloworld_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworldweb/helloworld_post/HelloworldPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworldweb.helloworld_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
